b2824b84a91c98ff22ae3821e00521f4f429564b
Fabian Eckelmann <EckelmannF@rki.de>
2026-03-25 12:49:03 +0100

    some small fixes and add linting to pre-commit

--- mex/editor/client/src/app/app.ts
+++ mex/editor/client/src/app/app.ts
@@ -1,23 +1,23 @@
-import { AsyncPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 class SampleData {
-  title = '';
-  text = '';
+  title = "";
+  text = "";
 }
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet, AsyncPipe],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected readonly title = signal('mex-editor');
+  protected readonly title = signal("mex-editor");
 
   private http = inject(HttpClient);
 
-  data$ = this.http.get<SampleData[]>('http://127.0.0.1:8000/api/v0/sample-data');
+  data$ = this.http.get<SampleData[]>("http://127.0.0.1:8000/api/v0/sample-data");
 }
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 import pytest
 from fastapi.testclient import TestClient
 
-from mex.editor.main import create_fastapi
+from mex.editor.testing import create_testing_api
 
 if TYPE_CHECKING:  # pragma: no cover
     from collections.abc import Generator
@@ -14,6 +14,6 @@ pytest_plugins = ("mex.common.testing.plugin",)
 @pytest.fixture(scope="session")
 def client() -> Generator[TestClient]:
     """Return a fastAPI test client initialized with our app."""
-    app = create_fastapi()
+    app = create_testing_api()
     with TestClient(app, raise_server_exceptions=False) as test_client:
         yield test_client
