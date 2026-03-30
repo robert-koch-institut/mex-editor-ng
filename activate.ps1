c633d1a4b438d0e1c0ced667e8bc01aead7dd87f
Fabian Eckelmann <EckelmannF@rki.de>
2026-03-30 09:48:08 +0200

    add activate and update lock

---
+++ activate.ps1
@@ -0,0 +1,5 @@
+.venv\Scripts\activate.ps1
+mex\editor\client\.nodeenv\Scripts\Activate.ps1
+$env:NODE_PATH="./mex/editor/client/node_modules"
+$env:NPM_CONFIG_PREFIX="./mex/editor/client"
+$env:PATH="./mex/editor/client/node_modules/.bin;$env:PATH"
