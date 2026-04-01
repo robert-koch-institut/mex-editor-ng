0e7731f368ba0c5158a29151c207ba4c129cc0dc
Fabian Eckelmann <EckelmannF@rki.de>
2026-04-01 10:57:45 +0200

    bye bye activate.ps

--- activate.ps1
+++
@@ -1,5 +0,0 @@
-.venv\Scripts\activate.ps1
-mex\editor\client\.nodeenv\Scripts\Activate.ps1
-$env:NODE_PATH="./mex/editor/client/node_modules"
-$env:NPM_CONFIG_PREFIX="./mex/editor/client"
-$env:PATH="./mex/editor/client/node_modules/.bin;$env:PATH"
