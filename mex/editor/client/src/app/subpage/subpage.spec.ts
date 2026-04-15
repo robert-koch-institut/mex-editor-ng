bc89904c201b165ee3345f538dc84e58d72cc1be
Nicolas Drebenstedt <drebenstedtn@rki.de>
2026-04-15 17:32:09 +0200

    fix tests, drop cdns

--- mex/editor/client/src/app/subpage/subpage.spec.ts
+++ mex/editor/client/src/app/subpage/subpage.spec.ts
@@ -1,5 +1,6 @@
 import type { ComponentFixture } from "@angular/core/testing";
 import { TestBed } from "@angular/core/testing";
+import { provideRouter } from "@angular/router";
 
 import { Subpage } from "./subpage";
 
@@ -10,6 +11,7 @@ describe("Subpage", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Subpage],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Subpage);
