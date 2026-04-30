ab9b94390ef7924a00f740027c7ce7758394564b
Fabian Eckelmann <EckelmannF@rki.de>
2026-04-30 09:57:24 +0200

    prepared for tutorial

--- mex/editor/client/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import type { Routes } from "@angular/router";
-import { Subpage } from "./subpage/subpage";
-
-export const routes: Routes = [
-  { component: Subpage, path: "subpage", data: { title: "Sub Page" } },
-  { component: Subpage, path: "sub/subpage", data: { title: "EVEN MORE Sub Page" } },
-];
--- mex/editor/client/src/app/app.ts
+++ mex/editor/client/src/app/app.ts
@@ -1,10 +1,6 @@
 import { AsyncPipe } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
 import { Component, inject, signal } from "@angular/core";
-import { MatButton } from "@angular/material/button";
-import { MatIcon } from "@angular/material/icon";
-import { MatSlideToggle } from "@angular/material/slide-toggle";
-import { RouterLink, RouterOutlet } from "@angular/router";
 
 interface PreviewItem {
   identifier: string;
@@ -19,24 +15,14 @@ interface PaginatedPreviewItems {
 @Component({
   selector: "app-root",
   imports: [
-    RouterOutlet,
-    RouterLink,
     AsyncPipe,
-    MatButton,
-    MatIcon,
-    MatSlideToggle,
   ],
   templateUrl: "./app.html",
   styleUrl: "./app.scss",
 })
 export class App {
   protected readonly title = signal("mex-editor-ng");
-
   private http = inject(HttpClient);
 
   data$ = this.http.get<PaginatedPreviewItems>("api/v0/backend/preview-item");
-
-  onClick() {
-    console.warn("CLICKED");
-  }
 }
